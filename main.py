606decc6d4d4e9671bda8d0a85804343e6392feb
John Henry <johnhmejia02@gmail.com>
2023-04-24 15:05:21 -0500

    eval metric
    eval metric

--- main.py
+++ main.py
@@ -15,8 +15,18 @@ X_train, X_test, Y_train, Y_test = preprocess(file_path)
 # Random Forest Regression model
 Y_pred = rand_forest(X_train, X_test, Y_train, Y_test)
 
-# Evaluate performance of model 1
-from sklearn.metrics import confusion_matrix, accuracy_score
-cm = confusion_matrix(Y_test, Y_pred)
-print(cm)
-accuracy_score(Y_test, Y_pred)
+# Evaluate performance of regression model by r^2 score
+from sklearn.metrics import r2_score
+print(r2_score(Y_test, Y_pred))
+
+# Evaluate performance of regression model by mean squared error
+from sklearn.metrics import mean_squared_error
+print(mean_squared_error(Y_test, Y_pred))
+
+# Visualize results
+plt.scatter(Y_test, Y_pred, color='blue')
+plt.title('Random Forest Regression')
+plt.xlabel('Actual')
+plt.ylabel('Predicted')
+plt.show()
+
