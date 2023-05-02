e7aac23dc3ad8efb53f8050f54d1696ff6803226
John Henry <johnhmejia02@gmail.com>
2023-05-01 19:59:20 -0500

    Update randomforest.R

--- randomforest.R
+++ randomforest.R
@@ -5,44 +5,72 @@ random_forest <- function(training_set, test_set, dataset) {
     # install.packages('randomForest')
 
   library(randomForest)
-  library(ggplot2)
+  library(plotly)
+  library(caret)
   
+  # Function to tune random forest with different values of hyperparameters and calculate R-squared
+  tune_random_forest <- function(training_set, test_set, ntree_range, mtry_range, max_depth_range) {
+    results <- list()
+    for (ntree in ntree_range) {
+      for (mtry in mtry_range) {
+        for (max_depth in max_depth_range) {
+          regressor <- randomForest(formula = victims ~ .,
+                                    data = training_set,
+                                    ntree = ntree,
+                                    mtry = mtry,
+                                    max_depth = max_depth)
+          y_pred <- predict(regressor, newdata = test_set)
+          r_squared <- R2(y_pred, test_set$victims)
+          results[[paste0("ntree_", ntree, "_mtry_", mtry, "_max_depth_", max_depth)]] <- r_squared
+        }
+      }
+    }
+    return(results)
+  }
+  
+  # Specify ranges of hyperparameters to test
+  ntree_range <- seq(50, 300, by = 50)
+  mtry_range <- seq(2, ncol(training_set) - 1, by = 2)
+  max_depth_range <- seq(2, 10, by = 2)
+  
+  # Call function to tune random forest and calculate R-squared for each combination of hyperparameters
+  results <- tune_random_forest(training_set, test_set, ntree_range, mtry_range, max_depth_range)
+  
+  # Convert results to data frame for plotting
+  result_df <- expand.grid(ntree = ntree_range, mtry = mtry_range, max_depth = max_depth_range)
+  result_df$r_squared <- unlist(results)
+  
+  # Plot R-squared vs hyperparameters using 3D surface plot
+  plot_ly(result_df, x = ~mtry, y = ~max_depth, z = ~r_squared, type = "surface",
+          colors = c("#d9f0a3", "#addd8e", "#78c679", "#41ab5d", "#238443", "#005a32")) %>%
+    layout(scene = list(xaxis = list(title = "mtry"),
+                        yaxis = list(title = "max_depth"),
+                        zaxis = list(title = "R-squared")))
+  
+  # Print best hyperparameters and corresponding R-squared value
+  best_hyperparameters <- result_df[which.max(result_df$r_squared), ]
+  best_ntree <- best_hyperparameters$ntree
+  best_mtry <- best_hyperparameters$mtry
+  best_max_depth <- best_hyperparameters$max_depth
+  best_r_squared <- best_hyperparameters$r_squared
+  print(paste0("Best hyperparameters:\nntree = ", best_ntree, "\nmtry = ", best_mtry,
+               "\nmax_depth = ", best_max_depth, "\nR-squared = ", best_r_squared))
+  
+  # Fit final random forest model using best hyperparameters
   regressor <- randomForest(formula = victims ~ .,
                             data = training_set,
-                            ntree = 100)
+                            ntree = best_ntree,
+                            mtry = best_mtry,
+                            max_depth = best_max_depth)
+  y_pred <- predict(regressor, newdata = test_set)
   
-  
-  
-  # Create a grid of values for the length of the track
-  x_grid <- seq(min(training_set$len), max(training_set$len), 0.1)
-  
-  
-  
-  # Plot the training data points and the predicted values from the random forest model
-  ggplot() +
-    geom_point(aes(x = training_set$len, y = training_set$victims),
-               colour = 'red') +
-    geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(len = x_grid))),
-              colour = 'blue') +
-    ggtitle('Random Forest Regression') +
-    xlab('Length of track') +
-    ylab('Number of victims')
-  
-  
-  library(ggplot2)
-  
-  ggplot(data = training_set) +
-    geom_point(aes(x = slon, y = slat, color = mag)) +
-    geom_line(aes(x = slon, y = slat, color = mag, group = interaction(mag, season)))
-  
+  # Print R-squared and mean squared error values for final model
+  r_squared <- R2(y_pred, test_set$victims)
+  mse <- mean((y_pred - test_set$victims)^2)
+  print(paste0("Final model R-squared: ", r_squared))
+  print(paste0("Final model mean squared: ", mse))
 
-    # Finding mean absolute error
 
-    y_pred = predict(regressor, newdata = test_set)
 
-    mean_absolute_error = mean(abs(y_pred - test_set$victims))
-
-    print(mean_absolute_error)
-    
 
 }
